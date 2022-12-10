c5d36fdeb197ef17918a7c2888b66e295c3e7542
Danil Fomenko <danilfom3124@gmail.com>
2022-12-10 14:35:04 +0200

    Added random and sdl2_image. Started programming game logic.

---
+++ src/game.rs
@@ -0,0 +1,166 @@
+use rand::prelude::*;
+
+pub const FIELD_WIDTH: usize = 4;
+pub const FIELD_HEIGHT: usize = 4;
+
+enum MoveDirection {
+    Left,
+    Right,
+    Top,
+    Bottom,
+}
+
+enum GameState {
+    WaitingForStart,
+    Playing,
+    Lose,
+    Win,
+}
+
+pub struct Game2048 {
+    playground: [[i32; FIELD_HEIGHT]; FIELD_WIDTH],
+    game_state: GameState,
+    rng: ThreadRng,
+}
+
+impl Game2048 {
+    pub fn new() -> Self {
+        Game2048 {
+            playground: [[0; FIELD_HEIGHT]; FIELD_WIDTH],
+            game_state: GameState::WaitingForStart,
+            rng: thread_rng(),
+        }
+    }
+    pub fn get_cell(&self, x: usize, y: usize) -> i32 {
+        assert!(x < FIELD_WIDTH);
+        assert!(y < FIELD_HEIGHT);
+
+        self.playground[x][y]
+    }
+    pub fn start(&mut self) {
+        assert!(matches!(self.game_state, GameState::WaitingForStart));
+        self.generate_and_place_random_block();
+    }
+    pub fn update(&mut self, move_direction: MoveDirection) {
+        // 1) try to move at least 1 block,
+        //    - if cannot then do nothing
+        //    - if can, then move every possible block
+        //
+        // 2) after movings, try to add random 2 or 4 block on empty cell
+        //
+        // 3) check if there's any possible move, if not then game is over
+        assert!(matches!(self.game_state, GameState::Playing));
+        if !self.can_move_in_direction(move_direction) {
+            return;
+        }
+        // Make move
+        // Generate random block
+        self.generate_and_place_random_block();
+    }
+    fn generate_and_place_random_block(&mut self) {
+        assert!(matches!(self.game_state, GameState::Playing));
+
+        let mut available_cells = Vec::<(usize, usize)>::new();
+
+        for y in 0..FIELD_HEIGHT {
+            for x in 0..FIELD_WIDTH {
+                if self.playground[x][y] == 0 {
+                    available_cells.push((x, y));
+                }
+            }
+        }
+
+        let (cell_x, cell_y) = available_cells.choose(&mut self.rng).unwrap();
+        let values = vec![2, 4];
+        let value = values.choose(&mut self.rng).unwrap();
+
+        self.playground[*cell_x][*cell_y] = *value;
+    }
+    fn can_move_in_direction(&self, move_direction: MoveDirection) -> bool {
+        match move_direction {
+            MoveDirection::Left => {
+                for i in 0..FIELD_HEIGHT {
+                    let mut line = Vec::<i32>::new();
+                    for x in 0..FIELD_WIDTH {
+                        let block = self.playground[x][i];
+                        line.push(block);
+                    }
+                    if Self::can_move_line(&line, false) {
+                        return true;
+                    }
+                }
+            }
+            MoveDirection::Right => {
+                for i in 0..FIELD_HEIGHT {
+                    let mut line = Vec::<i32>::new();
+                    for x in 0..FIELD_WIDTH {
+                        let block = self.playground[x][i];
+                        line.push(block);
+                    }
+                    if Self::can_move_line(&line, true) {
+                        return true;
+                    }
+                }
+            }
+            MoveDirection::Top => {
+                for i in 0..FIELD_WIDTH {
+                    let mut line = Vec::<i32>::new();
+                    for y in 0..FIELD_HEIGHT {
+                        let block = self.playground[i][y];
+                        line.push(block);
+                    }
+                    if Self::can_move_line(&line, false) {
+                        return true;
+                    }
+                }
+            }
+            MoveDirection::Bottom => {
+                for i in 0..FIELD_WIDTH {
+                    let mut line = Vec::<i32>::new();
+                    for y in 0..FIELD_HEIGHT {
+                        let block = self.playground[i][y];
+                        line.push(block);
+                    }
+                    if Self::can_move_line(&line, true) {
+                        return true;
+                    }
+                }
+            }
+        }
+        false
+    }
+    fn can_move_line(line: &Vec<i32>, forward: bool) -> bool {
+        // Here should be a big load of checks
+
+        let mut all_zero_check = false;
+        for el in line {
+            if *el != 0 {
+                all_zero_check = true;
+                break;
+            }
+        }
+        if !all_zero_check {
+            return false;
+        }
+
+        let mut line_full_with_diff_check = true;
+        let mut numbers = Vec::<i32>::new();
+        for el in line {
+            if *el == 0 {
+                break;
+            }
+            if numbers.contains(el) {
+                break;
+            }
+            numbers.push(*el);
+            if numbers.len() == line.len() {
+                line_full_with_diff_check = false;
+            }
+        }
+        if !line_full_with_diff_check {
+            return false;
+        }
+
+        true
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 extern crate sdl2;
 
 use sdl2::event::Event;
+use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
+use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
-use sdl2::rect::Rect;
-use sdl2::render::TextureQuery;
+use sdl2::rect::{Point, Rect};
+use sdl2::render::{Canvas, Texture, TextureCreator, TextureQuery};
+use sdl2::ttf::Font;
+use sdl2::video::{Window, WindowContext};
 use std::thread::sleep;
 use std::time::Duration;
+
+mod game;
 
 macro_rules! rect(
     ($x:expr, $y:expr, $w:expr, $h:expr) => (
@@ -14,12 +20,19 @@ macro_rules! rect(
     )
 );
 
+static FONT_NAME: &str = "Helvetica.ttf";
+
+static WINDOW_WIDTH: u32 = 800;
+static WINDOW_HEIGHT: u32 = 600;
+
+static SQUARE_SIZE: u32 = 64;
+
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem
-        .window("2048", 800, 600)
+        .window("2048", WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
         .opengl()
         .allow_highdpi()
@@ -29,12 +42,59 @@ fn main() -> Result<(), String> {
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
     let texture_creator = canvas.texture_creator();
     let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
+    let font = ttf_context.load_font(FONT_NAME, 64).unwrap();
+    // End of initialization
 
-    let text = "Hello Jabroni";
+    let game = game::Game2048::new();
+
+    let text = "The 2048";
+    let (texture, mut rectangle) = create_text(&texture_creator, &font, text, (20, 20));
+
+    let start_x = WINDOW_WIDTH - rectangle.width() / 2;
+    rectangle.x = start_x as i32;
+
+    // let texture = texture_creator.load_texture("square.png")?;
+    // let rectangle = rect!(30, 30, 64, 64);
+
+    let mut event_pump = sdl_context.event_pump()?;
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::MouseButtonDown {
+                    x,
+                    y,
+                    mouse_btn: MouseButton::Left,
+                    ..
+                } => {}
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        canvas.set_draw_color(Color::RGB(251, 206, 177));
+        canvas.clear();
+
+        canvas.copy(&texture, None, rectangle)?;
+
+        canvas.present();
+        sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+
+    Ok(())
+}
+
+fn create_text<'tc>(
+    texture_creator: &'tc TextureCreator<WindowContext>,
+    font: &Font,
+    text: &str,
+    point: (i32, i32),
+) -> (Texture<'tc>, Rect) {
     let color = Color::RGB(0, 0, 0);
-    let point = (10, 10);
 
-    let font = ttf_context.load_font("Helvetica.ttf", 64).unwrap();
     let surface = font
         .render(text)
         .blended(color)
@@ -50,26 +110,5 @@ fn main() -> Result<(), String> {
 
     let rectangle = rect!(point.0, point.1, width, height);
 
-    let mut event_pump = sdl_context.event_pump()?;
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
-        }
-        canvas.set_draw_color(Color::RGB(211, 211, 211));
-        canvas.clear();
-
-        canvas.copy(&texture, None, rectangle)?;
-
-        canvas.present();
-        sleep(Duration::new(0, 1_000_000_000u32 / 30));
-    }
-
-    Ok(())
+    (texture, rectangle)
 }
