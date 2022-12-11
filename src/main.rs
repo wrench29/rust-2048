e11137a17f9b5099c30f03cc5d999d135d1a9858
Danil Fomenko <danilfom3124@gmail.com>
2022-12-11 18:53:18 +0200

    Added tiles textures and their rendering

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
 use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
-use sdl2::rect::{Point, Rect};
-use sdl2::render::{Canvas, Texture, TextureCreator, TextureQuery};
+use sdl2::rect::Rect;
+use sdl2::render::{Texture, TextureCreator, TextureQuery};
 use sdl2::ttf::Font;
-use sdl2::video::{Window, WindowContext};
+use sdl2::video::WindowContext;
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -20,12 +20,12 @@ macro_rules! rect(
     )
 );
 
-static FONT_NAME: &str = "Helvetica.ttf";
+static FONT_NAME: &str = "assets/Helvetica.ttf";
 
 static WINDOW_WIDTH: u32 = 800;
 static WINDOW_HEIGHT: u32 = 600;
 
-static SQUARE_SIZE: u32 = 64;
+static SQUARE_SIZE: u32 = 160;
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init().unwrap();
@@ -51,11 +51,46 @@ fn main() -> Result<(), String> {
     let text = "The 2048";
     let (texture, mut rectangle) = create_text(&texture_creator, &font, text, (20, 20));
 
-    let start_x = WINDOW_WIDTH - rectangle.width() / 2;
-    rectangle.x = start_x as i32;
+    rectangle.x = (canvas.viewport().width() as i32 - rectangle.width() as i32) / 2;
 
-    // let texture = texture_creator.load_texture("square.png")?;
-    // let rectangle = rect!(30, 30, 64, 64);
+    let tile_2_texture = texture_creator.load_texture("assets/2-tile.png")?;
+    let tile_4_texture = texture_creator.load_texture("assets/4-tile.png")?;
+    let tile_8_texture = texture_creator.load_texture("assets/8-tile.png")?;
+    let tile_16_texture = texture_creator.load_texture("assets/16-tile.png")?;
+    let tile_32_texture = texture_creator.load_texture("assets/32-tile.png")?;
+    let tile_64_texture = texture_creator.load_texture("assets/64-tile.png")?;
+    let tile_128_texture = texture_creator.load_texture("assets/128-tile.png")?;
+    let tile_256_texture = texture_creator.load_texture("assets/256-tile.png")?;
+    let tile_512_texture = texture_creator.load_texture("assets/512-tile.png")?;
+    let tile_1024_texture = texture_creator.load_texture("assets/1024-tile.png")?;
+    let tile_2048_texture = texture_creator.load_texture("assets/2048-tile.png")?;
+
+    let canvas_width = canvas.viewport().width();
+
+    let (tile_x_1, tile_y_1) = calc_rectangle_position(0, 0, canvas_width);
+    let (tile_x_2, tile_y_2) = calc_rectangle_position(1, 1, canvas_width);
+    let (tile_x_3, tile_y_3) = calc_rectangle_position(2, 2, canvas_width);
+    let (tile_x_4, tile_y_4) = calc_rectangle_position(3, 3, canvas_width);
+
+    let tile00_rectangle = rect!(tile_x_1, tile_y_1, SQUARE_SIZE, SQUARE_SIZE);
+    let tile01_rectangle = rect!(tile_x_1, tile_y_2, SQUARE_SIZE, SQUARE_SIZE);
+    let tile02_rectangle = rect!(tile_x_1, tile_y_3, SQUARE_SIZE, SQUARE_SIZE);
+    let tile03_rectangle = rect!(tile_x_1, tile_y_4, SQUARE_SIZE, SQUARE_SIZE);
+
+    let tile10_rectangle = rect!(tile_x_2, tile_y_1, SQUARE_SIZE, SQUARE_SIZE);
+    let tile11_rectangle = rect!(tile_x_2, tile_y_2, SQUARE_SIZE, SQUARE_SIZE);
+    let tile12_rectangle = rect!(tile_x_2, tile_y_3, SQUARE_SIZE, SQUARE_SIZE);
+    let tile13_rectangle = rect!(tile_x_2, tile_y_4, SQUARE_SIZE, SQUARE_SIZE);
+
+    let tile20_rectangle = rect!(tile_x_3, tile_y_1, SQUARE_SIZE, SQUARE_SIZE);
+    let tile21_rectangle = rect!(tile_x_3, tile_y_2, SQUARE_SIZE, SQUARE_SIZE);
+    let tile22_rectangle = rect!(tile_x_3, tile_y_3, SQUARE_SIZE, SQUARE_SIZE);
+    let tile23_rectangle = rect!(tile_x_3, tile_y_4, SQUARE_SIZE, SQUARE_SIZE);
+
+    let tile30_rectangle = rect!(tile_x_4, tile_y_1, SQUARE_SIZE, SQUARE_SIZE);
+    let tile31_rectangle = rect!(tile_x_4, tile_y_2, SQUARE_SIZE, SQUARE_SIZE);
+    let tile32_rectangle = rect!(tile_x_4, tile_y_3, SQUARE_SIZE, SQUARE_SIZE);
+    let tile33_rectangle = rect!(tile_x_4, tile_y_4, SQUARE_SIZE, SQUARE_SIZE);
 
     let mut event_pump = sdl_context.event_pump()?;
     'running: loop {
@@ -75,10 +110,30 @@ fn main() -> Result<(), String> {
                 _ => {}
             }
         }
-        canvas.set_draw_color(Color::RGB(251, 206, 177));
+        canvas.set_draw_color(Color::RGB(200, 200, 200));
         canvas.clear();
 
         canvas.copy(&texture, None, rectangle)?;
+
+        canvas.copy(&tile_2_texture, None, tile00_rectangle)?;
+        canvas.copy(&tile_4_texture, None, tile01_rectangle)?;
+        canvas.copy(&tile_8_texture, None, tile02_rectangle)?;
+        canvas.copy(&tile_16_texture, None, tile03_rectangle)?;
+
+        canvas.copy(&tile_32_texture, None, tile10_rectangle)?;
+        canvas.copy(&tile_64_texture, None, tile11_rectangle)?;
+        canvas.copy(&tile_128_texture, None, tile12_rectangle)?;
+        canvas.copy(&tile_256_texture, None, tile13_rectangle)?;
+
+        canvas.copy(&tile_512_texture, None, tile20_rectangle)?;
+        canvas.copy(&tile_1024_texture, None, tile21_rectangle)?;
+        canvas.copy(&tile_2048_texture, None, tile22_rectangle)?;
+        canvas.copy(&tile_2_texture, None, tile23_rectangle)?;
+
+        canvas.copy(&tile_4_texture, None, tile30_rectangle)?;
+        canvas.copy(&tile_8_texture, None, tile31_rectangle)?;
+        canvas.copy(&tile_16_texture, None, tile32_rectangle)?;
+        canvas.copy(&tile_32_texture, None, tile33_rectangle)?;
 
         canvas.present();
         sleep(Duration::new(0, 1_000_000_000u32 / 60));
@@ -111,4 +166,16 @@ fn create_text<'tc>(
     let rectangle = rect!(point.0, point.1, width, height);
 
     (texture, rectangle)
+}
+
+fn calc_rectangle_position(x: u32, y: u32, canvas_width: u32) -> (u32, u32) {
+    let top_margin = 150;
+    let margin = 16;
+    let field_length = (SQUARE_SIZE * 4) + (margin * 3);
+    let left_first = (canvas_width - field_length) / 2;
+
+    let x_output = left_first + (x * (SQUARE_SIZE + margin));
+    let y_output = top_margin + (y * (SQUARE_SIZE + margin));
+
+    (x_output, y_output)
 }
